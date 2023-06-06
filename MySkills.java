b47abaa3f25f7fe9acd460bf07151c20164110d4
Erhn1 <111546581+Erhn1@users.noreply.github.com>
2023-06-06 17:18:30 +0800

    Update MySkills.java

--- MySkills.java
+++ MySkills.java
@@ -1,24 +1,16 @@
 package exam1;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-class MySkills extends Myself {
+public class MySkills extends Myself {
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Custom image path for MySkills class
-        ImageIcon imageIcon = new ImageIcon("C:\\Users\\Charles\\Downloads\\myskills.jpg");
+        // Custom image for MySkills class
+        ImageIcon imageIcon = new ImageIcon("C:\\Users\\Charles\\Downloads\\4.jpg");
 
-        // Call the desired methods inherited from the superclass to utilize their functionality
+        // Inherit methods from the superclass
         JFrame frame = createFrame("MySkills", 800, 600);
         JPanel contentPane = createPanel(frame.getSize(), new BorderLayout());
         JLabel imageLabel = createImageLabel(imageIcon, contentPane.getSize());
@@ -41,15 +33,15 @@ class MySkills extends Myself {
         return new JLabel(resizedImageIcon);
     }
 
+    // Override the fuction of the next button
     @Override
     protected JButton createNextButton() {
-        JButton nextButton = new JButton(">"); // Provide your custom button label
+        JButton nextButton = new JButton(">");
         nextButton.setBackground(Color.black);
         nextButton.setForeground(Color.white);
         nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Call the actionPerformed method of the MyWorks class directly
                 MyWorks myWorks = new MyWorks();
                 myWorks.actionPerformed(e);
             }
